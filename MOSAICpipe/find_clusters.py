32d83d698a7b472bb1a5f6b3503f133ec3f445de
Steven Boada <stevenboada@gmail.com>
2018-01-24 11:27:37 -0500

    add function to rebuild tracker file

--- MOSAICpipe/find_clusters.py
+++ MOSAICpipe/find_clusters.py
@@ -1,7 +1,7 @@
 from glob import glob
 import os
 import time
-
+import sys
 
 def mkCommands(dirs):
     cmds = []
@@ -33,6 +33,24 @@ def mkTracker(cmds, clobber=False):
         print('Tracker already exists. Call with clobber=True to overwrite')
         return
 
+def rebuildTracker(cmds, clobber=True):
+    imgs = glob('PSZ*/**/*A.png', recursive=True)
+    fields = [i.split('/')[0] for i in imgs] # the fields already done
+    if not os.path.isfile('tracker') or clobber:
+        with open('tracker', 'w') as tracker:
+            for cmd in cmds:
+                status = False
+                for i, f in enumerate(fields):
+                    if f in cmd:
+                        status = True
+                        break
+                if status:
+                    tracker.write('True {}\n'.format(cmd))
+                else:
+                    tracker.write('False {}\n'.format(cmd))
+
+        return
+
 def doWork():
     print('doing work....')
 
@@ -53,6 +71,10 @@ def doWork():
     with open('tracker', 'w') as tracker:
         for i, (done, cmd) in enumerate(zip(status, cmds)):
             if done == 'True':
+                print()
+                print()
+                print('{}/{} Fields completed.'.format(i + 1, fields))
+                print('Sleeping for 5 seconds. Press ctrl-c to exit')
                 continue
             if done == 'False':
                 print(cmd)
@@ -60,7 +82,7 @@ def doWork():
                 status[i] = 'True'
             print()
             print()
-            print('{}/{} Fields completed.'.format(i, fields))
+            print('{}/{} Fields completed.'.format(i + 1, fields))
             print('Sleeping for 5 seconds. Press ctrl-c to exit')
             try:
                 time.sleep(5)
@@ -71,7 +93,16 @@ def doWork():
                 return
 
 
-dirs = glob('./PS*', recursive=True)
-cmds = mkCommands(dirs)
-mkTracker(cmds)
-doWork()
+if __name__ == "__main__":
+    try:
+        if 'rebuild' in sys.argv[1].lower():
+            rebuild = True
+    except IndexError:
+        rebuild = False
+    dirs = glob('./PS*', recursive=True)
+    cmds = mkCommands(dirs)
+    mkTracker(cmds)
+    if rebuild:
+        rebuildTracker(cmds)
+    else:
+        doWork()
