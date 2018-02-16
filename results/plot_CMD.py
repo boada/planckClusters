061a3eae1c7b6b52f2c87e840b89642668e6ddc3
Steven Boada <stevenboada@gmail.com>
2018-02-16 15:12:06 -0500

    calculates different tau on demand

--- results/plot_CMD.py
+++ results/plot_CMD.py
@@ -6,21 +6,32 @@ from astropy.coordinates import SkyCoord
 import astropy.units as u
 from numpy import abs, linspace
 import ezgal # BC03 model maker
+import os
+# check to make sure we have defined the bpz filter path
+if not os.getenv('EZGAL_FILTERS'):
+    os.environ['EZGAL_FILTERS'] = ('/home/boada/Projects/planckClusters/'
+                                   'MOSAICpipe/bpz-1.99.3/FILTER/')
 
-def setup_models(zf):
-    model = ezgal.model('bc03_exp_0.1_z_0.02_salp.model')
+def setup_models(zf, tau):
+    #model = ezgal.model('bc03_exp_0.1_z_0.02_salp.model')
+    model = ezgal.model('bc03_ssp_z_0.02_chab.model')
+
+    if isinstance(tau, list):
+        raise ValueError('tau cannot be a list')
+    exp = model.make_exponential(tau)
+
     # set cosmology
-    model.set_cosmology(Om=0.3, Ol=0.7, h=0.7, w=-1)
+    exp.set_cosmology(Om=0.3, Ol=0.7, h=0.7, w=-1)
     # set the model normalization to Dai et al 2009 (ApJ, 697, 506)
-    model.set_normalization('ch1', 0.24, -25.06, vega=True)
-    model.add_filter('g_MOSAICII.res', name='g_MOSAICII_MAG_ISO')
-    model.add_filter('r_MOSAICII.res', name='r_MOSAICII_MAG_ISO')
-    model.add_filter('SLOAN-SDSS.i.res', name='i_MOSAICII_MAG_ISO')
-    model.add_filter('SLOAN-SDSS.z.res', name='z_MOSAICII_MAG_ISO')
-    model.add_filter('K_KittPeak.res', name='K_KittPeak_MAG_ISO')
-    model.set_zfs(zf)
+    exp.set_normalization('ch1', 0.24, -25.06, vega=True)
+    exp.add_filter('g_MOSAICII.res', name='g_MOSAICII_MAG_ISO')
+    exp.add_filter('r_MOSAICII.res', name='r_MOSAICII_MAG_ISO')
+    exp.add_filter('SLOAN-SDSS.i.res', name='i_MOSAICII_MAG_ISO')
+    exp.add_filter('SLOAN-SDSS.z.res', name='z_MOSAICII_MAG_ISO')
+    exp.add_filter('K_KittPeak.res', name='K_KittPeak_MAG_ISO')
+    exp.set_zfs(zf)
 
-    return model
+    return exp
 
 def find_nearest(array, value):
     ''' returns the index and value of the nearest value in an array. '''
@@ -29,11 +40,11 @@ def find_nearest(array, value):
 
 
 # get the models for BC03
-zf = [1, 1.5, 2, 2.5, 3]
-models = setup_models(zf)
+tau = [1, 1.25, 1.5, 1.75, 2]
+models = [setup_models(5, t) for t in tau]
 
 # make some colored lines for the plots
-cm_subset = linspace(0.2, 0.8, len(zf))
+cm_subset = linspace(0.2, 0.8, len(tau))
 colors = [cm.bone(x) for x in cm_subset]
 
 results = loadClusters()
@@ -99,15 +110,15 @@ for user in users:
                     c='#a60628', )# label='Cluster Galaxies')
 
             # add the model colors
-            for ii, color in zip(zf, colors):
-                mag1 = models.get_observed_absolute_mags(ii,
+            for ii, color in enumerate(colors):
+                mag1 = models[ii].get_observed_absolute_mags(5,
                                                         filters=filters[j],
-                                                        zs=z_cl)
-                mag2 = models.get_observed_absolute_mags(ii,
+                                                        zs=z_cl, ab=True)
+                mag2 = models[ii].get_observed_absolute_mags(5,
                                                         filters=filters[j + 1],
-                                                        zs=z_cl)
+                                                        zs=z_cl, ab=True)
                 ax[j].axhline(mag1 - mag2, color=color,
-                                  label='zf={}'.format(ii))
+                                  label='tau={}'.format(tau[ii]))
 
             ax[j].set_xlabel(filters[j + 1][0], fontsize=16) # filter letter
             ax[j].set_ylabel('{} - {}'.format(filters[j][0], filters[j +
@@ -123,6 +134,6 @@ for user in users:
         ax[0].legend(loc='lower left', ncol=2)
 
         plt.tight_layout()
-        plt.savefig('{}/{}/{}/{}_CMD.pdf'.format(user, c, c, c), bbox='tight')
+        plt.savefig('{}/{}/{}/{}_CMD.png'.format(user, c, c, c), bbox='tight')
         plt.close('all')
 
