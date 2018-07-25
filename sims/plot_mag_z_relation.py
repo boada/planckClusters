1fc80c6212d520117bf736ef2a8c1858558e2e8c
Steven Boada <stevenboada@gmail.com>
2018-07-25 14:06:09 -0400

    add completeness from models

--- sims/plot_mag_z_relation.py
+++ sims/plot_mag_z_relation.py
@@ -7,6 +7,7 @@ import cosmology
 import numpy
 import matplotlib.pyplot as plt
 import matplotlib
+from scipy.stats import linregress
 Polygon = matplotlib.patches.Polygon
 
 def mag_z(axes):
@@ -29,6 +30,61 @@ def mag_z(axes):
     axes.set_ylim(16.5, 26)
 
     return
+
+def calc_completeness_model(fields):
+    ''' Calculates the completeness using a histogram. '''
+
+    from astropy.io import ascii
+
+    bins = numpy.arange(15, 30, 0.5)
+    centers = (bins[:-1] + bins[1:]) / 2
+
+    data_dir = '../data/proc2_small/'
+
+    completeness_hist = []
+    for f in fields:
+        cat = '{}/{}/{}i_cal.cat'.format(data_dir, f, f)
+        cat = ascii.read(cat)
+        cat = cat.to_pandas()
+        cat = cat.loc[cat.MAG_AUTO < 40]
+        cat = cat.loc[cat.CLASS_STAR < 0.8]
+
+        # make a bunch of figures
+        n, bins_ = numpy.histogram(cat['MAG_AUTO'], bins=bins)
+
+        # make it a log plot
+        logn = numpy.log10(n)
+
+        # find the peak
+        peak = numpy.argmax(logn)
+
+        # make a model from mag 18.5 - 21.5
+        model = linregress(centers[peak - 5:peak], logn[peak - 5:peak])
+
+        # convert the linear model in lin-log space to log in linear space
+        #  and figure out where 80% completeness is
+        # see https://en.wikipedia.org/wiki/Semi-log_plot
+        y = n / (10**model.intercept * 10**(centers * model.slope))
+        x = centers
+
+        # plot(y, x) to see how the ratio curve goes.
+        func = interpolate.interp1d(x, y)
+
+        # the interpolate wasn't doing very well...
+        # when just asked what is 80%
+        mags = numpy.arange(centers[0], centers[-1], 0.1)
+        magdiff = 0.8 - func(mags)
+
+        # find the last bin where the difference is negative
+        # this is the bin, with the highest magnitude, where we go from having
+        # more observed objects to more objects in the model.
+        mag_idx = numpy.where(magdiff < 0)[0][-1]
+
+        completeness_hist.append(mags[mag_idx])
+
+        print(f, '{:.3f}'.format(mags[mag_idx]))
+
+    return completeness_hist
 
 def mag_lim_hist(axes):
     Ngal_o = 100
@@ -67,6 +123,22 @@ def mag_lim_hist(axes):
 
     axes.hist(completeness, bins=mag, color='#348abd')
     #axes.hist(completeness_low, bins=mag, color='#348abd')
+    axes.set_ylabel('$N_{fields}$')
+    axes.set_xlabel("$i'$ 80% Limit")
+
+def mag_lim_hist_model(axes):
+    m1 = 20.0
+    m2 = 25.0
+    dm = 0.2
+    mag = numpy.arange(m1, m2, dm)
+
+    # find all of the fields we have hunted
+    imgs = glob('../cluster_search/round2/PSZ*/**/*A.png', recursive=True)
+    fields = [i.split('/')[-2] for i in imgs]
+
+    completeness = calc_completeness_model(fields)
+
+    axes.hist(completeness, bins=mag, color='#348abd')
     axes.set_ylabel('$N_{fields}$')
     axes.set_xlabel("$i'$ 80% Limit")
 
@@ -131,7 +203,7 @@ if __name__ == "__main__":
     fig, axes = plt.subplots(ncols=2,
                              squeeze=True,
                              figsize=(7, 7 * (numpy.sqrt(5.) - 1.0) / 2.0))
-    mag_lim_hist(axes[0])
+    mag_lim_hist_model(axes[0])
     mag_z(axes[1])
 
     plt.tight_layout()
