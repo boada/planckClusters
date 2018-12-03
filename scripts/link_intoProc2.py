ff8f6011798dd170953c997e7c327a106f74f832
Steven Boada <stevenboada@gmail.com>
2018-12-03 13:32:28 -0500

    updated to better deal with k-band

--- scripts/link_intoProc2.py
+++ scripts/link_intoProc2.py
@@ -10,20 +10,19 @@ happen in the proc directory.
 
 '''
 
+# this tells us what sort of files to link. Resampled are the individual
+# exposures. We have both MOSAIC and NEWFIRM resampled images. Stacked are the
+# vo-created mosaics. We only have NEWFIRM stacked images.
+#file_type = 'resampled'
+file_type = 'stacked'
+
 # use sorted files to keep the masks behind
-files = sorted(glob('./vo_proc/201*/*/*.fz', recursive=True))
+files = sorted(glob('./vo_proc/201*/{}/*.fz'.format(file_type), recursive=True))
 
 for i, f in enumerate(files):
     # keep the stacked/resampled files seperate
     # do the step first as it's the easiest to exclude files
     path_parts = f.split('/')
-    if 'resampled' in f:
-        file_type = 'resampled'
-    elif 'stacked' in f:
-        file_type = 'stacked'
-        continue
-    else:
-        continue
 
     # don't take the original files
     if 'orig' in f:
@@ -55,8 +54,12 @@ for i, f in enumerate(files):
     if 'mosaic3' in inst.lower():
         inst = 'mosaic3'
 
+    if file_type == 'resampled':
+        if 'newfirm' in inst.lower():
+            continue
+
     # for linking into the proc dir
-    target_dir = './{}/{}'.format('proc2', obj)
+    target_dir = './{}/{}/tiles'.format('proc2', obj)
     if not os.path.isdir(target_dir):
         os.makedirs(target_dir)
 
@@ -75,14 +78,12 @@ for i, f in enumerate(files):
         # link the file name index +1
         new_file = files[i + 1]
         fname = new_file.split('/')[-1]
-        #fname = path_parts[-1]
-        #findex = fname.split('tu')[-1].rstrip('fits.fz')
-        #findex_new = int(findex) + 1
-        #fname = 'tu{}.fits.fz'.format(findex_new)
     elif 'k4' in path_parts[-1].lower():
         fname = path_parts[-1]
         if 'opi' in fname:
             fname = fname.replace('opi', 'opd')
+        elif 'osi' in fname:
+            fname = fname.replace('osi', 'osd')
         else:
             print('something is wrong!!! inner!!!')
     else:
