e976ee8334ace516955820f7680cfbe2b48a4eb1
Steven Boada <stevenboada@gmail.com>
2019-02-27 16:18:21 -0500

    update for full sdss catalog

--- scripts/getImagesSDSS.py
+++ scripts/getImagesSDSS.py
@@ -3,10 +3,11 @@ from astropy import coordinates as coords
 from astroquery.sdss import SDSS
 import os
 from itertools import cycle
+import sys
+sys.path.append(f'{os.environ["HOME"]}/Projects/planckClusters/catalogs')
+from load_catalogs import load_PSZcatalog
 
-# get file data
-data = np.genfromtxt('../../PSZ2_unconfirmed_catalog - Master.csv',
-           delimiter=',', names=True, dtype=None)
+data = load_PSZcatalog(unconf=True)
 
 # check to see if we are continuing
 try:
@@ -18,13 +19,10 @@ except FileNotFoundError:
 for i, (ra, dec, name) in enumerate(zip(data['RA'], data['Dec'],
                                         data['Name'])):
     # make string comparisons work
-    name = name.decode()
+    name = name.replace(' ', '_')
 
     # here's the little bit for persistance (see below)
     if i <= done:
-        continue
-    # only do the things in the footprint
-    if not data['SDSS_Footprint'][i].decode() == 'TRUE':
         continue
     print(name)
     if not os.path.isdir(name):
@@ -34,23 +32,24 @@ for i, (ra, dec, name) in enumerate(zip(data['RA'], data['Dec'],
     print('fetching images....')
     imgs = SDSS.get_images(pos, radius="20'", band='ugriz', data_release=12)
 
-    print('writing %s' % name, end='')
-    counter = 0
-    for HDU, band in zip(imgs, cycle('ugriz')):
-        print('.', end='')
-        if os.path.isfile(f'./{name}/{name}_sdss_{band}_{counter}.fits'):
-            HDU.writeto(f'./{name}/{name}_sdss_{band}_{counter + 1}.fits',
-                        clobber=True)
-            if band == 'z':
-                counter += 1
-        else:
-            HDU.writeto(f'./{name}/{name}_sdss_{band}_{counter}.fits',
-                        clobber=True)
+    if imgs:
+        print('writing %s' % name, end='')
+        counter = 0
+        for HDU, band in zip(imgs, cycle('ugriz')):
+            print('.', end='')
+            if os.path.isfile(f'./{name}/{name}_sdss_{band}_{counter}.fits'):
+                HDU.writeto(f'./{name}/{name}_sdss_{band}_{counter + 1}.fits',
+                            clobber=True)
+                if band == 'z':
+                    counter += 1
+            else:
+                HDU.writeto(f'./{name}/{name}_sdss_{band}_{counter}.fits',
+                            clobber=True)
 
-    # add a little thing to keep track how many we have done. Poor man's
-    # persistance
-    with open('imagesGot.txt', 'w') as f:
-        f.writelines(f'{i}')
+        # add a little thing to keep track how many we have done. Poor man's
+        # persistance
+        with open('imagesGot.txt', 'w') as f:
+            f.writelines(f'{i}')
 
     print('')
 
