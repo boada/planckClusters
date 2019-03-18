b72348a24c9184a12604e6885a79a1b5c77312ec
Steven Boada <stevenboada@gmail.com>
2019-03-18 10:09:52 -0400

    clean up and add pandas

--- scripts/getImagesPS1.py
+++ scripts/getImagesPS1.py
@@ -2,9 +2,10 @@ from time import sleep
 import subprocess
 import shlex
 import os
-from astropy.table import Table
-from numpy import append as npappend
+import sys
 from astLib.astCoords import decimal2hms, decimal2dms
+sys.path.append(f'{os.environ["HOME"]}/Projects/planckClusters/catalogs')
+from load_catalogs import load_PSZcatalog
 
 def check_redo(outpath, name):
     ''' check to see if we really need to do anything '''
@@ -28,7 +29,7 @@ def work(outpath, ra, dec, name):
     ra_sex = decimal2hms(ra, ':')
     dec_sex = decimal2dms(dec, ':')
 
-    cmd = 'panstamps --width=10 --filters grizy '
+    cmd = 'panstamps --width=40 --filters grizy '
     cmd += f'--downloadFolder={outpath}/{name} stack {ra_sex} {dec_sex}'
     print(cmd)
     p = subprocess.Popen(shlex.split(cmd))
@@ -46,83 +47,57 @@ def rename(outpath, name):
         print(f'{name} has no data!')
         return
 
-    filters = ['g', 'r', 'i', 'z', 'y']
-
-    for filt, f in zip(filters, files[0][2]):
+    for f in files[0][2]:
         if 'PSZ' in f:
             # don't need to rename
             continue
-        elif filt in f:
+        filt = f[6]
+
+        print(filt)
+
+        # make sure it's in our list
+        if filt not in ['g', 'r', 'i', 'z', 'y']:
+            print('problem')
+        else:
             os.rename(f'{outpath}/{name}/{f}',
                       f'{outpath}/{name}/{name}_PS1stack_{filt}.fits')
-        else:
-            print(f'Missing the {filt} filter!')
-
-def load_PSZcatalog():
-
-    datapath = './../../planckClusters/catalogs/'
-
-    ps1 = Table.read(f'{datapath}/PSZ1v2.1.fits')
-    ps2 = Table.read(f'{datapath}/PSZ2v1.fits')
-
-    # convert to pandas
-    df1 = ps1.to_pandas()
-    df2 = ps2.to_pandas()
-
-    # clean up strings -- not required
-    df1 = df1.applymap(lambda x: x.decode() if isinstance(x, bytes) else x)
-    df2 = df2.applymap(lambda x: x.decode() if isinstance(x, bytes) else x)
-
-    # merge the catalogs together
-    df_m = df1.merge(
-        df2,
-        how='outer',
-        left_on='INDEX',
-        right_on='PSZ',
-        suffixes=('_PSZ1', '_PSZ2'))
-
-    # get the columns that we want
-    cols = df_m.columns[[0, 1, 4, 5, 8, 29, 33, 34, 37, 38, 40, 51]]
-    df_final = df_m[cols]
-
-    # remerge to find bits that were missing
-    df_final_bigger = df_final.merge(
-        df2, how='left', left_on='INDEX_PSZ1', right_on='PSZ')
-    # fill in nans
-    for col in ['NAME', 'RA', 'DEC', 'SNR', 'REDSHIFT', 'INDEX']:
-        df_final_bigger[col + '_PSZ2'] = df_final_bigger[col + '_PSZ2'].fillna(
-            df_final_bigger[col])
-    # fill in nans
-    for col in ['NAME', 'RA', 'DEC', 'SNR', 'REDSHIFT', 'INDEX']:
-        df_final_bigger[col + '_PSZ2'] = df_final_bigger[col + '_PSZ2'].fillna(
-            df_final_bigger[col])
-    for col in ['NAME', 'RA', 'DEC']:
-        df_final_bigger[col] = df_final_bigger[col + '_PSZ2'].fillna(
-            df_final_bigger[col + '_PSZ1'])
-
-    df_final_bigger = df_final_bigger[npappend(
-        df_final_bigger.columns[:12].values, ['NAME', 'RA', 'DEC'])]
-
-    return df_final_bigger
 
 
 data = load_PSZcatalog()
 
-outpath = './../data/extern/PS1'
+outpath = './PS1'
+datapath = '../data/proc2'
 
-for i, (ra, dec, name) in enumerate(
-        zip(data['RA'], data['DEC'], data['NAME'])):
+for i, row in data.iterrows():
+    n = row.NAME.replace(' ', '_')
+    #print(n)
+    if os.path.isdir(f'{datapath}/{n}'):
+        name_us = n
+    else:
+        try:
+            n_psz1 = row.NAME_PSZ1.replace(' ', '_')
+        except AttributeError:
+            continue
+        if os.path.isdir(f'{datapath}/{n_psz1}'):
+            name_us = n_psz1
+        else:
+            continue
 
-    print(name)
-    name = name.replace(' ', '_')
+    ra = float(row.RA)
+    dec = float(row.DEC)
 
-    if dec < -31:
+#    if dec < -31:
+#        continue
+
+#    if not check_redo(outpath, f'{name_us}'):
+#        continue
+
+    if not os.path.isfile(f'{datapath}/{name_us}/{name_us}K.fits'):
         continue
 
-    if not check_redo(outpath, f'{name}'):
+    if os.path.isdir(f'{outpath}/{n}'):
         continue
 
-    work(outpath, ra, dec, f'{name}')
-    rename(outpath, f'{name}')
+    work(outpath, ra, dec, f'{n}')
+    rename(outpath, f'{n}')
     sleep(1)
-
